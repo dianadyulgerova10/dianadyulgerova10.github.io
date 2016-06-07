fa78efa3cb47edf850caf34a1f6a925da6acf7aa
dianadyulgerova10 <dianadyulgerova10@gmail.com>
2016-06-07 17:54:55 +0300

    responsive search

--- js/scripts.js
+++ js/scripts.js
@@ -12,5 +12,18 @@ $(document).ready(function() {
 		return false;
 	  
 	});
+	
+		var $search_link=$('#search_link');
+		var $nav_search=$('#nav_search')
+		
+	$search_link.click(function(event){
+		console.log('44');
+		// event.preventDefaut();
+		$nav_search.toggleClass('search_show');
+	
+	});
+	
+	  
+	
 
 });
