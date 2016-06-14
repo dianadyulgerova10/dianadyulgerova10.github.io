5fdeaec9f57d54d70a66b03aafd95b5cd73e8421
dianadyulgerova10 <dianadyulgerova10@gmail.com>
2016-06-14 16:20:26 +0300

    add 4,5

--- js/tasks/2/2.js
+++ js/tasks/2/2.js
@@ -83,5 +83,3 @@ thirdBit(10);
 thirdBit(0);
 thirdBit(5343);
 
-// Problem 6. Point in Circle
-// Write an expression that checks if given point P(x, y) is within a circle K({0,0}, 5). //{0,0} is the centre and 5 is the radius
