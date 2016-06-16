3350cd4d30a8dfcbebfd6a5fb8dea3c6ca8910d5
dianadyulgerova10 <dianadyulgerova10@gmail.com>
2016-06-16 18:13:49 +0300

    task 5

--- js/tasks/5/5.js
+++ js/tasks/5/5.js
@@ -14,8 +14,8 @@
 //      console.log(arr[i]);
 // }
 
-// // Problem 2. Lexicographically comparison
-// // Write a script that compares two char arrays lexicographically (letter by letter).
+// Problem 2. Lexicographically comparison
+// Write a script that compares two char arrays lexicographically (letter by letter).
 // console.log("Problem 2");
 
 // var arr1=["a","b","r","f","g"];
@@ -28,7 +28,7 @@
 //     console.log("arr2");
 //  }
 // else if (str1 < str2) {
-//             console.log("arr1 ");
+//             console.log("arr1");
 //  }
 // else {
 //   console.log("equal");
@@ -36,43 +36,111 @@
 
 // Problem 3. Maximal sequence
 // Write a script that finds the maximal sequence of equal elements in an array.
-console.log("Problem 3");
-var arr=[2, 1, 1, 2, 3, 3, 2, 2, 2, 2, 1, 3, 3],
-	cnt,
-	bigCnt=0,
-	number,
-	i,
-	len;
+// console.log("Problem 3");
+
+// var arr=[2, 1, 1, 2, 3, 3, 2, 2, 2, 2, 1, 3, 3],
+// 	cnt,
+// 	bigCnt=0,
+// 	number,
+// 	i,
+// 	len;
 	
-for(i=0, len=arr.length; i<len ; i += 1){
-	if(arr[i]==arr[i+1]){
-		cnt+=1;
-		if(bigCnt<cnt){
-			bigCnt=cnt;
-			number=arr[i];
-		}
-	}
-	else{
-		cnt=0;
-	}
-}
+// for(i=0, len=arr.length; i<len ; i += 1){
+// 	if(arr[i]==arr[i+1]){
+// 		cnt+=1;
+// 		if(bigCnt<cnt){
+// 			bigCnt=cnt;
+// 			number=arr[i];
+// 		}
+// 	}
+// 	else{
+// 		cnt=0;
+// 	}
+// }
 
-if(bigCnt==0){
-	console.log("no sequence");
-}
-else{
-	var a=number.toString() + " ";
-	console.log(a.repeat(bigCnt+1));
-}
+// if(bigCnt==0){
+// 	console.log("no sequence");
+// }
+// else{
+// 	var a=number.toString() + " ";
+// 	console.log(a.repeat(bigCnt+1));
+// }
 
-// Problem 4. Maximal increasing sequence
-// Write a script that finds the maximal increasing sequence in an array. / 3, 2, 3, 4, 2, 2, 4
-console.log("Problem 4")
+// // Problem 4. Maximal increasing sequence
+// // Write a script that finds the maximal increasing sequence in an array. / 3, 2, 3, 4, 2, 2, 4
+// console.log("Problem 4");
+
+// 
+// Problem 5. Selection sort
+// Sorting an array means to arrange its elements in increasing order.
+// Write a script to sort an array.
+// Use the selection sort algorithm: Find the smallest element, move it at the first position, find the smallest from the rest, move it at the second position, etc.
+// Hint: Use a second array
+// console.log("Problem 5");
+
+// var arr=[12,3,5,6,29,1,31,35,15],
+// 	smallest,
+// 	i,
+// 	j,
+// 	len,
+// 	index;
+
+// for(i=0, len=arr.length; i<len; i++){
+// 	index=i;
+// 	for(j=i+1, len=arr.length;j<len; j++){
+// 		if(arr[j]<arr[index]){
+// 			index=j;
+// 		}
+// 	}
+// 	smallest=arr[index];
+// 	arr[index]=arr[i];
+// 	arr[i]=smallest;
+// }
+// console.log(arr);
+
+// Problem 6. Most frequent number
+// Write a script that finds the most frequent number in an array.
+// console.log("Problem 6");
+
+// var arr=[5,67,3,23,6,3,1,6,3],
+// 	sortArr=arr.slice(),
+// 	cnt,
+// 	i,
+// 	j,
+// 	len,
+// 	index,
+// 	smallest,
+// 	bigCnt=0,
+// 	number=0;
+
+// for(i=0,len=sortArr.length; i<len; i++){
+// 	index=i;
+// 	for(j=i+1, len=sortArr.length; j<len; j++){
+// 		if(sortArr[j]<sortArr[index]){
+// 			index=j;
+// 		}
+// 	}
+// 	smallest=sortArr[index];
+// 	sortArr[index]=sortArr[i];
+// 	sortArr[i]=smallest;
+// }
+
+// for (i=0,len=sortArr.length; i<len; i++) {
+// 	if(sortArr[i]==sortArr[i+1]){
+// 		cnt++;
+// 		if(bigCnt<cnt){
+// 			bigCnt=cnt;
+// 			number=sortArr[i];
+// 		}
+// 	}
+// 	else{
+// 		cnt=0;
+// 	}
+// }
+// console.log(number + "("+ (bigCnt+1) + " times)");
 
 
-
-
-
-
+// Problem 7. Binary search
+// Write a script that finds the index of given element in a sorted array of integers by using the binary search algorithm.
 
 
