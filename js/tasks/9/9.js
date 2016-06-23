cb8fa722d279b6512ec831759ec2091b574e909e
dianadyulgerova10 <dianadyulgerova10@gmail.com>
2016-06-23 14:18:37 +0300

    task 9/add problem 4,5,6,7,8

--- js/tasks/9/9.js
+++ js/tasks/9/9.js
@@ -67,3 +67,110 @@ console.log(substringInText(text, "in"));
 // Note: Regions can be nested.
 console.log("Problem 4");
 
+var text="We are <mixcase>living</mixcase> in a <upcase>yellow submarine</upcase>. We <mixcase>don't</mixcase> have <lowcase>anything</lowcase> else.";
+
+function parseTags(str){
+	var newStr="";
+
+ 	for (var i = 0; i < str.length; i++){
+
+            if (str[i] + str[i+1]==='<u') {
+                i += 8;
+                while (str[i] +str[i+1] + str[i+2]!=='</u'
+                	) 
+                {	
+                    newStr += str[i].toUpperCase();
+                    i+=1;
+                }
+                i += 8;
+            }
+            else if (str[i] +str[i+1]==='<l') {
+                i+=9;
+                while (str[i]+str[i+1] + str[i+2]!=='</l') {
+                    newStr += str[i].toLowerCase();
+                    i+=1;
+                }
+                i+=9;
+            }
+            else if (str[i] +str[i+1] ==='<m') {
+                i += 9;
+                while (str[i]+str[i+1] + str[i+2] !=='</m') {
+                    if (i % 2===0) {
+                        newStr+=str[i].toUpperCase();
+                    } else {
+                        newStr+=str[i].toLowerCase();
+                    }
+                    i+= 1;
+                }
+                i+= 9;
+            }
+       		else{
+            	newStr+=str[i];
+            }
+    }
+return newStr;
+}
+
+console.log(parseTags(text));
+
+// Problem 5. nbsp
+// Write a function that replaces non breaking white-spaces in a text with &nbsp;
+console.log("Problem 5");
+var text="A B C D";
+
+function replaceWhiteSpaces(str){
+	var regex = new RegExp(" ", 'gi');
+    return text.replace(regex, "&nbsp")
+}
+
+console.log(replaceWhiteSpaces(text));
+
+// Problem 6. Extract text from HTML
+// Write a function that extracts the content of a html page given as text.
+// The function should return anything that is in a tag, without the tags.
+console.log("Problem 6");
+
+function textFromHtml(htmlText){
+	return htmlText.textContent;
+}
+console.log(textFromHtml(document.documentElement));
+
+// Problem 7. Parse URL
+// Write a script that parses an URL address given in the format: [protocol]://[server]/[resource] and extracts from it the [protocol], [server] and [resource] elements.
+// Return the elements in a JSON object.
+console.log("Problem 7");
+
+function parseURL(url){
+
+	var parse={ protocol: url.match(/(http(s)?)/gi),
+				server: url.match(/:\/\/(.*?)\//)[1],
+				resource: url.match(/[a-zA-Z](\/.*)/)[1],
+	urlToString : function(){
+			return "{ protocol: "+ this.protocol+ ", server: " + this.server + ", resource: " + this.resource + " }"; 
+		}
+	}
+		return parse.urlToString();
+	}
+
+console.log(parseURL("http://telerikacademy.com/Courses/Courses/Details/239"));
+
+// Problem 8. Replace tags
+// Write a JavaScript function that replaces in a HTML document given as string all the tags <a href="…">…</a> with corresponding tags [URL=…]…/URL].
+console.log("Problem 8");
+
+var text='<p>Please visit <a href="http://academy.telerik.com">our site</a> to choose a training course. Also visit <a href="www.devbg.org">our forum</a> to discuss the courses.</p>'
+
+function replaceTags(str){
+
+	while(str.indexOf('<a href') != -1) {
+        str = str.replace('<a href="', '[URL=')
+       			 .replace('">', ']')
+        		 .replace('</a>', '[/URL]');
+    }
+    return str;
+}
+
+console.log(replaceTags(text));
+
+
+
