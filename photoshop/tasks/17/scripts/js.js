11a03bd7e641965a8b3aff14866589aead84c3cd
dianadyulgerova10 <dianadyulgerova10@gmail.com>
2016-07-08 18:00:24 +0300

    task 17

--- photoshop/tasks/17/scripts/js.js
+++ photoshop/tasks/17/scripts/js.js
@@ -12,8 +12,18 @@ $(document).ready(function() {
 				 		})
 	};
 
+
+
 	dropDown($('#clothing'),$('#clothing .drop-down'));	
 	dropDown($('#brands'),$('#brands .drop-down'));
 	dropDown($('#journal'),$('#journal .drop-down'));
+
+
+	$('#search').click(function(){
+			$('#search .drop-down').css({"display": "block", "top":23 ,  "width" : 664});
+			// $('#search .drop-down').removeAttr(('left'))
+		})
+
+	
 
 })
