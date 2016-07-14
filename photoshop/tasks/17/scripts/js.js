42c0d67c937f70c92e5804b2b732afbef29b9860
dianadyulgerova10 <dianadyulgerova10@gmail.com>
2016-07-14 12:33:36 +0300

    task 17

--- photoshop/tasks/17/scripts/js.js
+++ photoshop/tasks/17/scripts/js.js
@@ -1,26 +1,24 @@
 $(document).ready(function() {
 
 
-
+// drop-down-menu
     function dropDown(menu, drop) {
         menu.mouseover(function() {
                 drop.css("display", "block");
-
             })
             .mouseout(function() {
                 drop.css("display", "none");
             })
-
-
     };
-
-
 
     dropDown($('#clothing'), $('#clothing .drop-down-categories'));
     dropDown($('#brands'), $('#brands .drop-down-categories'));
     dropDown($('#journal'), $('#journal .drop-down-categories'));
     dropDown($('#search'), $('.drop-down-search'));
+ 
+ // end drop-down-menu
 
+// carousel
 
     $('.glyphicon-chevron-left').mouseover(function() {
             $(this).css('background', 'url(images/arrow-rollover-click.png)')
@@ -36,5 +34,17 @@ $(document).ready(function() {
         .mouseout(function() {
             $(this).css('background', 'url(images/arrow-rollover.png)')
         })
+
+// end carousel
+
+
+// banner
+
+    $('#banner-close').click(function() {
+            $('#banner').css("display", "none");
+        })
+    
+// end banner
+
 
 })
