1c2b7ba79fbf43271b666302440f0ebffad2d196
dianadyulgerova10 <dianadyulgerova10@gmail.com>
2016-07-15 17:32:13 +0300

    task 17 photoshop

--- photoshop/tasks/17/scripts/js.js
+++ photoshop/tasks/17/scripts/js.js
@@ -1,38 +1,38 @@
 $(document).ready(function() {
 
 
-// drop-down-menu
+    // drop-down-menu
     function dropDown(menu) {
         menu.mouseover(function() {
                 $(this).children(".drop-down-categories").css("display", "block");
-                $(this).children(".drop-down-arrow").css("display","block");
+                $(this).children(".drop-down-arrow").css("display", "block");
 
             })
             .mouseout(function() {
                 $(this).children(".drop-down-categories").css("display", "none");
-                $(this).children(".drop-down-arrow").css("display","none");
+                $(this).children(".drop-down-arrow").css("display", "none");
             })
     };
 
     dropDown($('#clothing'));
     dropDown($('#brands'));
     dropDown($('#journal'));
-  
-//search drop-down
-$('#search').click(function(){
-		$('.drop-down-search').css("display","block");
-		$('.drop-down-arrow-search').css("display","block");
 
-	});
+    //search drop-down
+    $('#search').click(function() {
+        $('.drop-down-search').css("display", "block");
+        $('.drop-down-arrow-search').css("display", "block");
 
-	$('#search-x').click(function(){
-		 event.stopPropagation();
-		$('.drop-down-search').css("display","none");
-		$('.drop-down-arrow-search').css("display","none");
-	}); 
+    });
+
+    $('#search-x').click(function() {
+        event.stopPropagation();
+        $('.drop-down-search').css("display", "none");
+        $('.drop-down-arrow-search').css("display", "none");
+    });
 
 
-// carousel
+    // carousel
 
     $('.glyphicon-chevron-left').mouseover(function() {
             $(this).css('background', 'url(images/arrow-rollover-click.png)')
@@ -49,16 +49,16 @@ $('#search').click(function(){
         });
 
 
-// banner
+    // banner
     $('#banner-close').click(function() {
-            $('#banner').css("display", "none");
-        });
+        $('#banner').css("display", "none");
+    });
 
 
-//close video
-	$('.close-video').click(function(){
-		$('.video').css("display","none");
-	});
+    //close video
+    $('.close-video').click(function() {
+        $('.video').css("display", "none");
+    });
 
 
 
