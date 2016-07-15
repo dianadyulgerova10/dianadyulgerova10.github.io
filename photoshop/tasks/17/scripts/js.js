d22f56b9485aa8bbd5fe275979700096d49f972b
dianadyulgerova10 <dianadyulgerova10@gmail.com>
2016-07-15 16:47:19 +0300

    task 17

--- photoshop/tasks/17/scripts/js.js
+++ photoshop/tasks/17/scripts/js.js
@@ -2,21 +2,35 @@ $(document).ready(function() {
 
 
 // drop-down-menu
-    function dropDown(menu, drop) {
+    function dropDown(menu) {
         menu.mouseover(function() {
-                drop.css("display", "block");
+                $(this).children(".drop-down-categories").css("display", "block");
+                $(this).children(".drop-down-arrow").css("display","block");
+
             })
             .mouseout(function() {
-                drop.css("display", "none");
+                $(this).children(".drop-down-categories").css("display", "none");
+                $(this).children(".drop-down-arrow").css("display","none");
             })
     };
 
-    dropDown($('#clothing'), $('#clothing .drop-down-categories'));
-    dropDown($('#brands'), $('#brands .drop-down-categories'));
-    dropDown($('#journal'), $('#journal .drop-down-categories'));
-    dropDown($('#search'), $('.drop-down-search'));
- 
- // end drop-down-menu
+    dropDown($('#clothing'));
+    dropDown($('#brands'));
+    dropDown($('#journal'));
+  
+//search drop-down
+$('#search').click(function(){
+		$('.drop-down-search').css("display","block");
+		$('.drop-down-arrow-search').css("display","block");
+
+	});
+
+	$('#search-x').click(function(){
+		 event.stopPropagation();
+		$('.drop-down-search').css("display","none");
+		$('.drop-down-arrow-search').css("display","none");
+	}); 
+
 
 // carousel
 
@@ -25,26 +39,27 @@ $(document).ready(function() {
         })
         .mouseout(function() {
             $(this).css('background', 'url(images/arrow-rollover.png)')
-        })
-
+        });
 
     $('.glyphicon-chevron-right').mouseover(function() {
             $(this).css('background', 'url(images/arrow-rollover-click.png)')
         })
         .mouseout(function() {
             $(this).css('background', 'url(images/arrow-rollover.png)')
-        })
-
-// end carousel
+        });
 
 
 // banner
-
     $('#banner-close').click(function() {
             $('#banner').css("display", "none");
-        })
-    
-// end banner
+        });
+
+
+//close video
+	$('.close-video').click(function(){
+		$('.video').css("display","none");
+	});
+
 
 
 })
